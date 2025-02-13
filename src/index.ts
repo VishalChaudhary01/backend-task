b6cc72da069a9924ca62c6c02a1ddfb529b318ea
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-02-13 22:28:13 +0530

    database connection done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
+import { connectDB } from "./config/db";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
